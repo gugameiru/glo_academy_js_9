fb88cad017d038f471ccb1818409b26712045d8f
gugameiru <gugameiru@gmail.com>
2019-08-24 02:11:33 +0300

    Второе ДЗ - обязательное

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
-let money;
-let income;
-let addExpences;
-let deposit;
-let mission;
-let period;
+let money = 20000;
+let income = 'Фриланс';
+let addExpenses = '400, 500, 600';
+let deposit = false;
+let mission = 30000;
+let period = 12;
+let budgetDay = money / 30;
 
-alert ('Hello world!!!');
-
-console.log('Hello Max! Hellow Andrey!');
+console.log('money:', typeof money);
+console.log('income:', typeof income);
+console.log('deposit:', typeof deposit);
+console.log('Длина строки income:', income.length);
+console.log('Период: ' + period + ' месяцев');
+console.log('Цель заработать: ' + mission + ' рублей');
+console.log(addExpenses.toLowerCase().split(', '));
+console.log('Дневной бюджет: ' + budgetDay.toFixed(2) + ' , остаток от деления: ' + money%30);
