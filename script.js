47935ec7997f942805f0e979716bc1e490547cd9
gugameiru <gugameiru@gmail.com>
2019-08-28 03:14:04 +0300

    Обязательное ДЗ номер 4

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ let income = 'Фриланс',
 let money = +prompt('Ваш месячный доход?', 'Введите сумму');
 
 let addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую:', 'Введите наименования расходов');
-console.log(addExpenses.toLowerCase().split(', '));
+
 
 let ifAnyDeposit = prompt('Есть ли у вас депозит в банке?', 'Ответьте Да или Нет');
 
@@ -18,16 +18,15 @@ if (ifAnyDeposit=='Да') {
     deposit = false;
 } 
 
-console.log('money:', typeof money);
-console.log('income:', typeof income);
-console.log('deposit:', typeof deposit);
+let showTypeOf = function(data) {
+    return (typeof(data));
+};
 
 let nesessaryExpenses1, 
     nesessaryExpenses2,
     nesessaryExpensesSum1,
     nesessaryExpensesSum2;
     
-
 for (let i = 0; i < 2; i++) {
     if (nesessaryExpenses1 != undefined) {
         nesessaryExpenses2 = prompt('Какие обязательные ежемесячные расходы у вас есть?', 'Введите наименование');
@@ -41,35 +40,64 @@ for (let i = 0; i < 2; i++) {
     }
 }
 
-let budgetMonth = money - nesessaryExpensesSum1 - nesessaryExpensesSum2;
-console.log('Доход за месяц: ' + budgetMonth + ' рублей');
+let getExpensesMonth = function() {
+    return nesessaryExpensesSum1 + nesessaryExpensesSum2;
+};
 
-let timeToMission;
+let getAccumulatedMonth = function() {
+    let expenses = getExpensesMonth();
+    return money - expenses;
+};
 
-// Проверка на деление на ноль:
+let getAccumulatedPeriod = function() {
+    let period = +prompt('Период накопления', 'Введите количество месяцев');
+    return [period, (getAccumulatedMonth() * period)];
+};
 
-budgetMonth != 0 ? timeToMission=Math.ceil(mission / budgetMonth) : timeToMission = 0;
+let getTargetMonth = function() {
+    let budgetMonth = getAccumulatedMonth();
+    let timeToMission;
+    // Проверка на деление на ноль:
+    (budgetMonth != 0) ? (timeToMission=Math.ceil(mission / budgetMonth)) : (timeToMission = 0);
+    return timeToMission;
 
+};
+
+let budgetDay = Math.floor(getAccumulatedMonth()/30);
+
+let getStatusIncome = function(){
+    switch (true) {
+        case (budgetDay > 800):
+            return ('Высокий уровень дохода');
+            
+        case (budgetDay > 300 && budgetDay <= 800):
+            return ('Средний уровень дохода');
+            
+        case (budgetDay >= 0 && budgetDay <= 300):
+            return ('Низкий уровень дохода');
+            
+        default:
+            return ('Что-то пошло не так');
+            
+    
+    }
+};
+
+console.log('Переменная "money" равна ' + money + ', ' + 'её тип: ' + showTypeOf(money));
+console.log('Переменная "income" равна ' + income + ', ' + 'её тип: ' + showTypeOf(income));
+console.log('Переменная "deposit" равна ' + deposit + ', ' + 'её тип: ' + showTypeOf(deposit));
+
+console.log(getStatusIncome());
+
+let periodToOutput = getAccumulatedPeriod();
+// Проверка на возможность накоплений
+if (periodToOutput[1] > 0) {
+    console.log('Накопления за ' + periodToOutput[0] + ' месяцев составят: ' + periodToOutput[1]);
+} else {
+    console.log('Накопления за ' + periodToOutput[0] + ' месяцев равны 0 ');
+}
+
+let target = getTargetMonth();
 // Вывод сообщения о недостижимости цели вместо отрицательного
 // количества месяцев, а то непонятно:
-
-timeToMission > 0 ? console.log('Цель будет достигнута через: '+ timeToMission + ' месяцев') : console.log('Цель недостижима');
-
-let budgetDay = Math.floor(budgetMonth/30);
-console.log('Бюджет на день: '+ budgetDay + ' рублей');
-
-switch (true) {
-    case (budgetDay > 800):
-        console.log('Высокий уровень дохода');
-        break;
-    case (budgetDay > 300 && budgetDay <= 800):
-        console.log('Средний уровень дохода');
-        break;
-    case (budgetDay >= 0 && budgetDay <= 300):
-        console.log('Низкий уровень дохода');
-        break;
-    default:
-        console.log('Что-то пошло не так');
-        break;
-
-}
+target > 0 ? console.log('Цель будет достигнута через: '+ target + ' месяцев') : console.log('Цель недостижима');
