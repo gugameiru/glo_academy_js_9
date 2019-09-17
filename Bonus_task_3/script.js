92fff266491cca5796481679262caad146a9e26d
gugameiru <gugameiru@gmail.com>
2019-09-17 23:23:32 +0300

    Сделано бонусное задание 3

---
+++ Bonus_task_3/script.js
@@ -0,0 +1,18 @@
+const text = document.querySelector('#color'),
+    button = document.querySelector('#change');
+
+const getRandom = () => {
+    let random =  Math.floor((Math.random()*256));
+    //Здесь реализован "красивый вывод", чтобы цифр после решетки всегда было 6
+    if ((random < 10) || ((random > 10) && (random < 16))) {
+        return ('0' + random.toString(16));
+    }else{
+        return random.toString(16);
+    }
+};
+
+button.addEventListener('click', () => {
+    let string = '#' + getRandom() + getRandom() + getRandom();
+    document.body.style.backgroundColor = button.style.color = string;
+    text.textContent = string;
+});
